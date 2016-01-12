a60d63bbe2e06ea222aba00223e4a012592d7e14
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-01-12 15:29:45 +0100

    Add super hero table view cell

---
+++ KataSuperHeroes/SuperHeroTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  SuperHeroTableViewCell.swift
+//  KataSuperHeroes
+//
+//  Created by Pedro Vicente Gomez on 12/01/16.
+//  Copyright © 2016 GoKarumi. All rights reserved.
+//
+
+import Foundation
+import BothamUI
+import SDWebImage
+
+class SuperHeroTableViewCell: UITableViewCell, BothamViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+
+    func configureForItem(item: SuperHero) {
+        nameLabel.text = item.name
+        nameLabel.accessibilityLabel = item.name
+        photoImageView.sd_setImageWithURL(item.photo)
+        applyImageGradient(photoImageView)
+    }
+
+    private func applyImageGradient(thumbnailImage: UIImageView) {
+        guard thumbnailImage.layer.sublayers == nil else {
+            return
+        }
+        let gradient: CAGradientLayer = CAGradientLayer(layer: thumbnailImage.layer)
+        gradient.frame = thumbnailImage.bounds
+        gradient.colors = [UIColor.gradientStartColor.CGColor, UIColor.gradientEndColor.CGColor]
+        gradient.startPoint = CGPointMake(0.0, 0.6)
+        gradient.endPoint = CGPointMake(0.0, 1.0)
+        thumbnailImage.layer.insertSublayer(gradient, atIndex: 0)
+    }
+
+}
