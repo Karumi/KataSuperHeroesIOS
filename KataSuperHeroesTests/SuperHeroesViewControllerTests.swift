df6bb0e3a36e30fe40630ee7e295e91ea1b5a1bf
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-01-13 10:44:59 +0100

    Configure KIF

---
+++ KataSuperHeroesTests/SuperHeroesViewControllerTests.swift
@@ -0,0 +1,19 @@
+//
+//  SuperHeroesViewControllerTests.swift
+//  KataSuperHeroes
+//
+//  Created by Pedro Vicente Gomez on 13/01/16.
+//  Copyright © 2016 GoKarumi. All rights reserved.
+//
+
+import Foundation
+import KIF
+import Nimble
+@testable import KataSuperHeroes
+
+class SuperHeroesViewControllerTests: AcceptanceTestCase {
+
+    func test() {
+        expect(true).to(equal(true))
+    }
+}
