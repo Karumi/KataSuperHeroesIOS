b87f0aec28b6e0608b6d2b16d4aadf726c47bd2b
Sergio Gutiérrez <Serchinastico@users.noreply.github.com>
2019-05-08 18:49:14 +0200

    Add accessibility label to loading view so that test called testDoNotShowLoadingViewIfThereAreSomeSuperHeroes makes sense (#12)

--- KataSuperHeroes/LoadingView.swift
+++ KataSuperHeroes/LoadingView.swift
@@ -21,6 +21,7 @@ open class LoadingView: UIView {
         view.frame = bounds
         view.autoresizingMask = [UIView.AutoresizingMask.flexibleWidth, UIView.AutoresizingMask.flexibleHeight]
         addSubview(view)
+        accessibilityLabel = "LoadingView"
     }
 
     private func loadViewFromNib() -> UIView {
